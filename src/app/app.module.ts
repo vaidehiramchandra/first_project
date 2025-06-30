b9dfe7cd06b231cc0e3fecedb20843c360a8333e
vaidehiramchandra <vaidehiramchandra@gmail.com>
2025-06-30 09:40:31 +0530

    Small Changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarInsComponent } from './motor/car-ins/car-ins.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CarInsComponent
   ],
   imports: [
     BrowserModule,
