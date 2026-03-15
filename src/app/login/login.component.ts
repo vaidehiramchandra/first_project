16e696365c5186db76351d6a3c3089cc3d3bfe09
vaidehiramchandra <vaidehiramchandra@gmail.com>
2026-03-15 16:16:55 +0530

    Login page created

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+  public isLogin:boolean = false
+
+  login(){
+    this.isLogin = true
+  }
+
+}
